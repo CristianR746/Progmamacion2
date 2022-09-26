43bff0397293dc09ff0a0f9c964e13df042e3309
CristianR746 <114274126+CristianR746@users.noreply.github.com>
2022-09-26 13:25:02 -0500

    hola

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+readme = open("./README.md", "r")
+
+
+setup(
+    name='CircuitoProgram',
+    packages=['CircuitoProgram'],  # this must be the same as the name above
+    version='0.1',
+    description='Esta es la descripcion de mi paquete',
+    long_description=readme.read(),
+    long_description_content_type='text/markdown',
+    author='Cristian Rivera, karen Pajon, Marcos David',
+    # use the URL to the github repo
+    url='https://github.com/CristianR746/Progmamacion2.git',
+    download_url='https://github.com/CristianR746/Progmamacion2.git',
+    keywords=['github','testing', 'logging', 'example'],
+    classifiers=[ ],
+    license='MIT',
+    include_package_data=True
+)
